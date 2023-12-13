a6b63e043b5d6e2901d37838137e09e81d4aeaf7
floralgreen <carducci.federico20@gmail.com>
2023-12-13 11:49:13 +0100

    Esercizio Completato

--- src/ErrorHandler.java
+++ src/ErrorHandler.java
@@ -2,18 +2,10 @@ public class ErrorHandler {
 
     public void confrontaCarattereENumero(char carattere, int numero) {
 
-        try {
-            if ((int) carattere == numero) {
-                System.out.println("Carattere " + carattere + " e " + numero + " sono uguali");
-
-            } else {
-                throw new Exception();
-            }
-        } catch (Exception e) {
-            System.out.println("Catturata Exception " + e);
-        } finally {
-            System.out.println("Carattere convertito in numero: " + (int) carattere);
-            System.out.println("Numero utilizzato per il confronto: " + numero);
+        if ((int) carattere == numero) {
+            System.out.println("Carattere " + carattere + " e " + numero + " sono uguali");
+        } else {
+            throw new ArithmeticException("Il carattere ed il numero non sono uguali");
         }
 
     }
